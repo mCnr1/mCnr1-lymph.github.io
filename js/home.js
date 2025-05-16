db5437ab30858c4fa03cd8eeccca10f080887cac
mCnr1 <130003291+mCnr1@users.noreply.github.com>
2025-05-16 17:24:44 -0500

    Initialized Project

---
+++ js/home.js
@@ -0,0 +1,6 @@
+// window.addEventListener('scroll', function () {
+//     const scrollY = window.scrollY;
+//     const diagram = document.querySelector('.diagram');
+//     diagram.style.transform = `translateY(${scrollY * 0.2}px)`;
+//   });
+  
